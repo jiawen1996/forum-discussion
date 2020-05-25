f4d11e6b803154d568a9bee87afe89193bf97769
lyujiawe <jiawen.lyu@etu.utc.fr>
2020-05-25 21:03:17 +0200

    forum.inverse = false, user.inverse = true

--- forum-discussion/src/com/sr03/forumdiscussion/dao/impl/ForumDAOImpl.java
+++ forum-discussion/src/com/sr03/forumdiscussion/dao/impl/ForumDAOImpl.java
@@ -90,14 +90,13 @@ public class ForumDAOImpl implements IForumDAO<Forum> {
 
 	}
 
-	public static List<Forum> FindAll(User owner) throws IOException, ClassNotFoundException, SQLException {
+	public static List<Forum> FindAll() throws IOException, ClassNotFoundException, SQLException {
 		Session session = factory.openSession();
 		Transaction tx = null;
 		try {
 			tx = session.beginTransaction();
-			String hql = "from Forum where owner = ?";
+			String hql = "from Forum";
 			Query query = session.createQuery(hql, Forum.class);
-			query.setParameter(0, owner);
 			List<Forum> listForums = query.getResultList();
 			tx.commit();
 			return listForums;
